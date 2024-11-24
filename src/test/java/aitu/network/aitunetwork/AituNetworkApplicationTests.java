bfa03c317fe246f62f2f3e5ddebb16f553fdf4d8
Yernur Omirzak <reluckytryhrd@gmail.com>
2024-11-25 00:26:28 +0500

    first commit

---
+++ src/test/java/aitu/network/aitunetwork/AituNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package aitu.network.aitunetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AituNetworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
