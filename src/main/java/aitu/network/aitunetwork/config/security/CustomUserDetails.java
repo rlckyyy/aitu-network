daa75bc063e1497ffc79b97fff0e1248c95b9563
Yernur Omirzak <reluckytryhrd@gmail.com>
2025-03-22 02:54:30 +0500

    test pipeline

--- src/main/java/aitu/network/aitunetwork/config/security/CustomUserDetails.java
+++ src/main/java/aitu/network/aitunetwork/config/security/CustomUserDetails.java
@@ -34,23 +34,4 @@ public class CustomUserDetails implements UserDetails {
         return user.getEmail();
     }
 
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
-    }
 }
