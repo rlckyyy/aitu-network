19a135084e0e41060fad04fce8821b986e0b8643
Atabek Bexeiit <bexeiitatabek@yandex.kz>
2025-04-25 15:13:31 +0500

    Add: check url filter

---
+++ src/main/java/aitu/network/aitunetwork/controller/CheckUrlFilter.java
@@ -0,0 +1,22 @@
+package aitu.network.aitunetwork.controller;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Slf4j
+@Component
+public class CheckUrlFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        log.warn("request url: {}", ((HttpServletRequest) servletRequest).getRequestURI());
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+}
