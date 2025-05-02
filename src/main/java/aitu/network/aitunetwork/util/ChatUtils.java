0d0bd47d896ed3e76e27ebd757282986a28d9f0b
Atabek Bexeiit <bexeiitatabek@yandex.kz>
2025-05-02 17:52:08 +0500

    Add: chat utils test, validation for ids size in one to one chat id generation

--- src/main/java/aitu/network/aitunetwork/util/ChatUtils.java
+++ src/main/java/aitu/network/aitunetwork/util/ChatUtils.java
@@ -5,6 +5,9 @@ import java.util.stream.Collectors;
 
 public class ChatUtils {
     public static String generateOneToOneChatId(Collection<String> ids) {
+        if (ids.size() != 2) {
+            throw new UnsupportedOperationException("Only two ids is valid for one to one chat id");
+        }
         return ids.stream()
                 .sorted()
                 .collect(Collectors.joining("_"));
--- src/test/java/aitu/network/aitunetwork/util/ChatUtilsTest.java
+++ src/test/java/aitu/network/aitunetwork/util/ChatUtilsTest.java
@@ -5,8 +5,24 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ChatUtilsTest {
+
+    @Test
+    void generateOneToOneChatId_shouldThrowUOE_whenThreeIdsPassed() {
+        String id1 = "id1";
+        String id2 = "id2";
+        String id3 = "id3";
+
+        List<String> ids = List.of(id1, id2, id3);
+        UnsupportedOperationException exception = assertThrows(
+                UnsupportedOperationException.class,
+                () -> ChatUtils.generateOneToOneChatId(ids),
+                "Unsupported operation exception should be thrown when ids size is not 2");
+        assertThat(exception.getMessage(), equalTo("Only two ids is valid for one to one chat id"));
+    }
 
     @Test
     void generateOneToOneChatId_shouldReturnSameChatId_whenIdsPassedWithTwoPossibleWays() {
