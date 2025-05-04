6f6c8552830cc841ec771dd8f01e330614036d9a
Atabek Bexeiit <bexeiitatabek@yandex.kz>
2025-05-04 17:35:03 +0500

    Add: video messages with preview

--- src/main/java/aitu/network/aitunetwork/config/SecurityConfig.java
+++ src/main/java/aitu/network/aitunetwork/config/SecurityConfig.java
@@ -42,6 +42,7 @@ public class SecurityConfig {
             "/api/v1/users/public/**",
             "/api/v1/file/**",
             "/api/v1/post/public/**",
+            "/error"
     };
 
     @Bean
--- src/main/java/aitu/network/aitunetwork/model/enums/MessageType.java
+++ src/main/java/aitu/network/aitunetwork/model/enums/MessageType.java
@@ -3,6 +3,7 @@ package aitu.network.aitunetwork.model.enums;
 public enum MessageType {
     MESSAGE_TEXT(false),
     MESSAGE_AUDIO(true),
+    MESSAGE_VIDEO(true),
     JOIN(false),
     LEAVE(false);
 
